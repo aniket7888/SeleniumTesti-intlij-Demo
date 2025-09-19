f145072f4cd8fef77306ce5b08d7eb475dfcc1a0
aniket7888 <aniketadmane9222@gmail.com>
2025-09-19 15:35:55 +0530

    commit

---
+++ src/main/java/PrintStringExample.java
@@ -0,0 +1,7 @@
+public class PrintStringExample {
+    public static void main(String[] args) {
+        String message = "Hello, Java!";
+        System.out.println(message); // Prints "Hello, Java!" and a new line
+        System.out.println("This is another line.");
+    }
+}
